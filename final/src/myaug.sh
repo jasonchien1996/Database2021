64188e278f75f60c7669f22384922cf92737e146
jasonchien1996 <43934912+jasonchien1996@users.noreply.github.com>
2021-06-29 19:19:11 +0800

    Update myaug.sh

--- final/src/myaug.sh
+++ final/src/myaug.sh
@@ -4,4 +4,4 @@ java Parser config
 g++ -shared -fPIC plugin.cpp myaug.cpp -o plugin.so -I /usr/local/include/opencv4 -lopencv_core -lopencv_imgproc -lopencv_imgcodecs
 sudo cp plugin.so /usr/lib/mysql/plugin/plugin.so
 systemctl restart mysql
-#python3 plug_func.py
+python3 plug_func.py
